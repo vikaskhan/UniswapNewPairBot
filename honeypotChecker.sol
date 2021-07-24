cef320c1c01608cab1821676acf5e5d021b03808
Vikas Khan <vikas.aslam.khan@gmail.com>
2021-07-24 00:50:13 -0400

    Added smart contract to check for honeypots

---
+++ honeypotChecker.sol
@@ -0,0 +1,42 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+interface IUniswapRouter {
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+}
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+}
+
+contract honeypotChecker {
+    
+    IUniswapRouter uniswapRouter;
+    
+    address WETH;
+    
+    constructor(address _UniswapRouter, address _WETH) {
+        uniswapRouter = IUniswapRouter(_UniswapRouter); 
+        WETH = _WETH;
+    }
+    
+    
+    function check (address token) external {
+        IERC20(WETH).approve(address(uniswapRouter), 1000000000000);
+        address[] memory path = new address[](2); 
+        path[0] = WETH; 
+        path[1] = token; 
+        uniswapRouter.swapExactTokensForTokens(1000000000000, 0, path, address(this), block.timestamp + 15);
+        uint amount = IERC20(token).balanceOf(address(this)); 
+        IERC20(token).approve(address(uniswapRouter), amount);
+        path[0] = token; 
+        path[1] = WETH; 
+        uniswapRouter.swapExactTokensForTokens(amount, 0, path, address(this), block.timestamp + 15); 
+    }
+}
